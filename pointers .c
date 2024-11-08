917fd880ccbec42b24ebbc458fa2815d3dbe4d76
KORUPALLI SAI NAVYA SRI <skorupal@student.gitam.edu>
2024-11-08 11:35:26 +0530

    Update pointers .c

--- pointers .c
+++ pointers .c
@@ -14,3 +14,31 @@ int main() {
 Value of ptr_i : 3806651476 and value pointed by the pointer is : 2024112431
 
 === Code Execution Successful ===*/
+
+
+
+# include <stdio.h>
+
+int main() {
+    int num=202411;
+    int*ptr=&num;
+    
+    printf("address of num=%u\n",&num);
+    printf("value   of ptr=%u\n",ptr);
+   
+    printf("address of ptr=%u\n",&ptr);
+    printf( "num=%d\n",*(&num));
+     printf( "num=%d\n",*(ptr));
+      printf( "num=%d\n",num);
+    
+    return 0;
+}
+/*address of num=1737697916
+value   of ptr=1737697916
+address of ptr=1737697904
+num=202411
+num=202411
+num=202411
+
+
+=== Code Execution Successful ===*/
